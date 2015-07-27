0beb90aec63b89a9e5d95b7a0e8566c7f5e0914e
Sashko Stubailo <sashko@stubailo.com>
2015-07-27 10:38:02 -0700

    Update versions

--- tutorial-registry/package.js
+++ tutorial-registry/package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'tutorial-registry',
-  version: '0.0.1',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'Register tutorials in a central location',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/meteor/tutorial-tools',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: '../docs/tutorial-registry.md'
@@ -17,10 +17,4 @@ Package.onUse(function(api) {
   ]);
   api.versionsFrom('1.1.0.2');
   api.addFiles('tutorial-registry.jsx');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('tutorial-registry');
-  api.addFiles('tutorial-registry-tests.js');
 });
