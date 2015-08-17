96d2248da3a3ae5d2b915fd59bafed19ddd12e9f
Dotan Simha <dotansimha@gmail.com>
2015-08-17 17:03:43 +0300

    Fixed a bug caused by any other commit messages without ":"

--- tutorial-step-diff-compiler/package.js
+++ tutorial-step-diff-compiler/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mdg:tutorial-step-diff-compiler',
-  version: '0.1.0',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'Build plugin that parses git patches',
   // URL to the Git repository containing the source code for this package.
