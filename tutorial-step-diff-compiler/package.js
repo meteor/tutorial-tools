34f7f221b12fe32a01a737984aab40d14c438692
Sashko Stubailo <sashko@stubailo.com>
2015-08-18 18:36:44 -0700

    Bump to 0.2.0 with different output format

--- tutorial-step-diff-compiler/package.js
+++ tutorial-step-diff-compiler/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mdg:tutorial-step-diff-compiler',
-  version: '0.1.1',
+  version: '0.2.0',
   // Brief, one-line summary of the package.
   summary: 'Build plugin that parses git patches',
   // URL to the Git repository containing the source code for this package.
