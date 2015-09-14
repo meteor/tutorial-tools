0053610cdeccadc09b3e722ce4b93d35756ce419
Sashko Stubailo <sashko@stubailo.com>
2015-09-14 10:03:36 -0700

    Bump version for publish

--- tutorial-diff-box/package.js
+++ tutorial-diff-box/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mdg:tutorial-diff-box',
-  version: '0.2.2',
+  version: '0.3.0',
   // Brief, one-line summary of the package.
   summary: 'A template to display parsed Git diffs as code snippets',
   // URL to the Git repository containing the source code for this package.
